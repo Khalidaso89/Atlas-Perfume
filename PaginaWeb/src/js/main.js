2cd9d62a5c22452184fd2720a57c307234b637d0
Khalid <karkid@institutmvm.cat>
2025-03-27 14:40:48 +0100

    1.8 añadiendo funciones y mejorando esteticas

--- PaginaWeb/src/js/main.js
+++ PaginaWeb/src/js/main.js
@@ -1,28 +1,42 @@
 
 //ICONO BUSQUEDA
-document.getElementById('icono-busqueda').onclick = function(i) {
+document.getElementById('icono-busqueda').onclick = function (i) {
     i.preventDefault();
     const buscador = document.getElementById('buscador');
     buscador.style.display = (buscador.style.display === 'block') ? 'none' : 'block';
 };
 
+
+//Botones de paises: aparecer / desaparecer
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.btn-country');
-    
+    let activeSection = null; // Variable para rastrear la sección activa
+
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const country = button.dataset.country;
+            const selectedSection = document.querySelector(`.country-section[data-country="${country}"]`);
 
-            // Ocultar todas las secciones
-            document.querySelectorAll('.country-section').forEach(section => {
-                section.classList.remove('active');
-                section.classList.add('d-none');
-            });
+            // Si la sección activa es la misma que la seleccionada, la ocultamos
+            if (activeSection === selectedSection) {
+                selectedSection.classList.remove('active');
+                activeSection = null;
+            } else {
+                // Ocultar todas las secciones
+                document.querySelectorAll('.country-section').forEach(section => {
+                    section.classList.remove('active');
+                });
 
-            // Mostrar la sección seleccionada
-            const selectedSection = document.getElementById(country);
-            selectedSection.classList.remove('d-none');
-            selectedSection.classList.add('active');
+                // Mostrar la nueva sección
+                selectedSection.classList.add('active');
+                activeSection = selectedSection;
+            }
         });
     });
 });
+
+
+
+
+//Flechas carrusel
+
